6b221cd4c841d655e5c20ce254121f55e9a0152f
Shruti Peragani <sangeetaperagani8642@gmail.com>
2024-10-03 00:13:36 +0530

    Adding frontend

---
+++ frontend/src/hooks/useGetCompanyById.js
@@ -0,0 +1,27 @@
+import { setCompany } from '@/redux/companySlice';
+import { COMPANY_API_END_POINT } from '@/utils/constants';
+import axios from 'axios';
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux';
+
+const useGetCompanyById = (companyid) => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const fetchACompany = async () => {
+      try {
+        const company = await axios.get(`${COMPANY_API_END_POINT}/get/${companyid}`, {
+          withCredentials: true,
+        });
+        // console.log(job.data.job);
+        if (company.data.success) {
+          dispatch(setCompany(company.data.company));
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchACompany();
+  }, [dispatch,companyid]);
+}
+
+export default useGetCompanyById
